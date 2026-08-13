c33b4f9dbd523741cafd5fef48c87033c0faae3e
DANIEL DAGNONI <daniel.dagnoni2@senaisp.edu.br>
2026-08-12 22:24:39 -0300

    chore: painel-defeitos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [risco, setRisco] = useState(0);
+  const [dimensao, setDimensao] = useState(0);
+  const [trinca, setTrinca] = useState(0);
+
+
+  const total = risco + dimensao + trinca;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      
+
+      <div className="item-defeito">
+        <span>Risco na superfície - <strong>{risco}</strong></span>
+        <button className="botao-registrar" onClick={() => setRisco(risco + 1)}>
+          Registrar
+        </button>
+      </div>
+
+      <div className="item-defeito">
+        <span>Dimensão fora do padrão - <strong>{dimensao}</strong></span>
+        <button className="botao-registrar" onClick={() => setDimensao(dimensao + 1)}>
+          Registrar
+        </button>
+      </div>
+
+      <div className="item-defeito">
+        <span>Peça trincada - <strong>{trinca}</strong></span>
+        <button className="botao-registrar" onClick={() => setTrinca(trinca + 1)}>
+          Registrar
+        </button>
+      </div>
+
+      <div className="mensagem-rodape">
+        {total === 0 
+          ? "Nenhum defeito registrado até o momento." 
+          : `Total de defeitos registrados: ${total}`
+        }
+      </div>
+
     </div>
   );
 }
